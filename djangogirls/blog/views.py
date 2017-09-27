81fb0f0095e2cf71bc4b06755c0ff129a4c3e247
Kyeong Lee <kaythechemist@gmail.com>
2017-09-27 22:06:40 +0900

    views settings def post_list rendering

--- djangogirls/blog/views.py
+++ djangogirls/blog/views.py
@@ -1,6 +1,11 @@
-from django.http import HttpResponse
 from django.shortcuts import render
+
+from .models import Post
 
 
 def post_list(request):
-    return HttpResponse('Post list')
+    posts = Post.objects.all()
+    context = {
+        'posts': posts,
+    }
+    return render(request, 'blog/post_list.html', context)
